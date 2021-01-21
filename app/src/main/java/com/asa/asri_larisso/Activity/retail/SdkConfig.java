bc8bd92a2c64688cc069a8bb6a5d39bcffb8db8a
sfynsss <sofyansss378@gmail.com>
2021-01-21 17:29:57 +0700

    Update

--- app/src/main/java/com/asa/asri_larisso/Activity/retail/SdkConfig.java
+++ app/src/main/java/com/asa/asri_larisso/Activity/retail/SdkConfig.java
@@ -2,7 +2,7 @@ package com.asa.asri_larisso.Activity.retail;
 
 public class SdkConfig {
 
-    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://asarasa.id/checkout.php/";
+    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://larisso.co.id/checkout.php/";
 //    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://192.168.1.15:8000/coba/";
     public static final String MERCHANT_CLIENT_KEY = "SB-Mid-client-D8pY_4poADw3T9SL";
 
